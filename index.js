309bb62f77ed2db0f125dc8b4d0f114166f0789a
Greg Aragon <garocdev@gmail.com>
2014-07-05 08:20:16 +0000

    Updated index.js for new html code

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var express = require('express')
-var app = express();
 var fs = require('fs')
-var buffer = new Buffer(fs.readFileSync("./index.html"));
+var htmlfile = "index.html"
 
-
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send(buffer.toString("utf-8"))
-
-  response.send(buffer.toString('utf-8'))
+   var html = fs.readFileSync(htmlfile).toString();
+   response.send(html);
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log("Listening on " + port)
 })
